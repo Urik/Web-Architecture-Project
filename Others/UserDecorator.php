d4cf8417555de3eeafc3b8f518b839e7fc53e15d
Uri Berman <uriberman@gmail.com>
2012-09-20 22:10:24 -0300

    Fix

--- Others/UserDecorator.php
+++ Others/UserDecorator.php
@@ -1,30 +1,35 @@
 <?php
+
 require("IUser.php");
+
 class UserDecorator implements IUser {
-	private $user;
-	
-	function __construct($user) {
-		$this->user = $user;
-	}
-	
-	public function getId() {
-			return $user->id;
-		}
-		
-	public function getNick() {
-		return $user->nick;
-	}
-	
-	public function getPassword() {
-		return $user->password;
-	}
-	
-	public function getEmail() {
-		return $user->email;
-	}
-	
-	public function getBirthday() {
-		return $user->email;
-	}
+    /* @var $user User */
+    private $user;
+
+    function __construct($user) {
+        $this->user = $user;
+    }
+
+    public function getId() {
+        return $user->id;
+    }
+
+    public function getNick() {
+        return $user->nick;
+    }
+
+    public function getPassword() {
+        return $user->password;
+    }
+
+    public function getEmail() {
+        return $user->email;
+    }
+
+    public function getBirthday() {
+        return $user->email;
+    }
+
 }
+
 ?>
--- Others/user.php
+++ Others/user.php
@@ -1,35 +1,58 @@
 <?php
+
+require_once "Thing.php";
 require("IUser.php");
-class User implements IUser{
-		private $id = 0;
-		private $nick = "";
-		private $password = "";
-		private $email = "";
+
+class User extends Thing implements IUser {
+
+    /**
+     * @var $id integer
+     * @var $nick string
+     * @var $password string
+     */
+    
+    private $id = 0;
+    private $nick = "";
+    /* @var string */
+    private $password = "";
+    private $email = "";
+
 //		private $birthday = getdate();
-		
-		function __construct($user, $password) {
-			$this->nick = $user;
-			$this->password = $password;
-		}
-		
-		public function getId() {
-			return $this->id;
-		}
-		
-		public function getNick() {
-			return $this->nick;
-		}
-		
-		public function getPassword() {
-			return $this->password;
-		}
-		
-		public function getEmail() {
-			return $this->email;
-		}
-		
-		public function getBirthday() {
-			return $this->email;
-		}
+
+    /**
+     * 
+     * @param string $user
+     * @param char[] $password
+     * @param type $email
+     * @param type $birthday
+     */
+    function __construct($user, $password, $email, $birthday) {
+        $this->nick = $user;
+        $this->password = $password;
+    }
+
+    public function getId() {
+        
+        return $this->id;
+    }
+
+    public function getNick() {
+        return $this->nick;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getBirthday() {
+        return $this->email;
+    }
+
 }
+
+
 ?>
